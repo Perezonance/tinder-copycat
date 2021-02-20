dfff7540bc70d31d672cdcf8a8bf5018b2ea2f1d
Alex Perez <manuel.perez@accesscfa.com>
2021-02-20 04:19:49 -0500

    seperates Config struct into config.go

--- profile-management-service/cmd/config.go
+++ profile-management-service/cmd/config.go
@@ -1,39 +1,39 @@
 package main
 
-// import "time"
+import "time"
 
-// /*Config is the structure used for configuring the server and its
-// dependencies*/
-// type Config struct {
+/*Config is the structure used for configuring the server and its
+dependencies*/
+type Config struct {
 
-// 	//Server provides server config options
-// 	Server struct {
-// 		//MockStorage is a boolean to determine whether to mock the storage solution or not
-// 		MockStorage bool `yaml:"mockStorage"`
+	//Server provides server config options
+	Server struct {
+		//MockStorage is a boolean to determine whether to mock the storage solution or not
+		MockStorage bool `yaml:"mockStorage"`
 
-// 		//Host is the local machine IP Address to bidn the HTTP Server to
-// 		Host string `yaml:"host"`
+		//Host is the local machine IP Address to bidn the HTTP Server to
+		Host string `yaml:"host"`
 
-// 		//Port is the local machine TCP Port to bind the HTTP Server to
-// 		Port string `yaml:"port"`
+		//Port is the local machine TCP Port to bind the HTTP Server to
+		Port string `yaml:"port"`
 
-// 		//Timeout is used for all timeout properties for the server
-// 		Timeout struct {
-// 			/*Server is the general server timeout to use for graceful
-// 			shutdowns*/
-// 			Server time.Duration `yaml:"server"`
+		//Timeout is used for all timeout properties for the server
+		Timeout struct {
+			/*Server is the general server timeout to use for graceful
+			shutdowns*/
+			Server time.Duration `yaml:"server"`
 
-// 			/*Write is the amount of time to wait until an HTTP Server
-// 			write operation is cancelled*/
-// 			Write time.Duration `yaml:"write"`
+			/*Write is the amount of time to wait until an HTTP Server
+			write operation is cancelled*/
+			Write time.Duration `yaml:"write"`
 
-// 			/*Read is the amount of time to wait until an HTTP server
-// 			read opertaion is cancelled*/
-// 			Read time.Duration `yaml:"read"`
+			/*Read is the amount of time to wait until an HTTP server
+			read opertaion is cancelled*/
+			Read time.Duration `yaml:"read"`
 
-// 			/*Idle is the amount of time to wait until and IDLE HTTP
-// 			Session is closed*/
-// 			Idle time.Duration `yaml:"idle"`
-// 		} `yaml:"timeout"`
-// 	} `yaml:"server"`
-// }
+			/*Idle is the amount of time to wait until and IDLE HTTP
+			Session is closed*/
+			Idle time.Duration `yaml:"idle"`
+		} `yaml:"timeout"`
+	} `yaml:"server"`
+}
--- profile-management-service/cmd/main.go
+++ profile-management-service/cmd/main.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/Perezonance/tinder-copycat/profile-management-service/internal/controllers"
 	"github.com/Perezonance/tinder-copycat/profile-management-service/internal/server"
@@ -17,47 +16,6 @@ import (
 	"github.com/gorilla/mux"
 	"gopkg.in/yaml.v2"
 )
-
-const (
-	//TODO: Pull from Config
-	timeoutDurVal = time.Second * 15
-)
-
-/*Config is the structure used for configuring the server and its
-dependencies*/
-type Config struct {
-
-	//Server provides server config options
-	Server struct {
-		//MockStorage is a boolean to determine whether to mock the storage solution or not
-		MockStorage bool `yaml:"mockStorage"`
-
-		//Host is the local machine IP Address to bidn the HTTP Server to
-		Host string `yaml:"host"`
-
-		//Port is the local machine TCP Port to bind the HTTP Server to
-		Port string `yaml:"port"`
-
-		//Timeout is used for all timeout properties for the server
-		Timeout struct {
-			/*Server is the general server timeout to use for graceful
-			shutdowns*/
-			Server time.Duration `yaml:"server"`
-
-			/*Write is the amount of time to wait until an HTTP Server
-			write operation is cancelled*/
-			Write time.Duration `yaml:"write"`
-
-			/*Read is the amount of time to wait until an HTTP server
-			read opertaion is cancelled*/
-			Read time.Duration `yaml:"read"`
-
-			/*Idle is the amount of time to wait until and IDLE HTTP
-			Session is closed*/
-			Idle time.Duration `yaml:"idle"`
-		} `yaml:"timeout"`
-	} `yaml:"server"`
-}
 
 //NewConfig returns a new decoded Config struct
 func newConfig(configPath string) (*Config, error) {
